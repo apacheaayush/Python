6a4ee6899c248d9fb6c2cbb02b4581cfc363349d
Aayush aryan <aayush.aryan91@gmail.com>
2017-09-09 14:13:46 +0530

    Add files via upload

---
+++ Coding Examples/1. Greatest Common Divisor/Greatest Common Divisor.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Basic Algorithm of finding out GCD of two numbers Using few lists   
+   
+def gcd(m,n):
+    fm = []
+    for i in range(1, m+1):
+        if (m%i) == 0:
+            fm.append(i)
+            
+    fn = []
+    for i in range(1, n+1):
+        if (n%i) == 0:
+            fn.append(i)
+            
+    cf = []
+    for i in fm:
+        if i in fn:
+            cf.append(i)
+    print("Results from gcd")
+    print(cf[-1])
+    
+gcd(14, 63)
+gcd(9999, 100000)
+gcd(20000, 35608)
+
+
+# Do we need three lists?
+
+def gcd1(m, n):
+    cf = []
+    for i in range(1, min(m,n) + 1): 
+        # GCD will always be less than or equal to the smaller number.
+        if (m % i) == 0 & (n % i) == 0:
+            cf.append(i)
+    print("Results from gcd1")
+    print(cf[-1])
+    
+gcd1(14, 63)
+gcd1(9999, 100000)
+gcd1(20000, 35608)
+
+
+# Do we need lists at all? Scan Backwards.
+
+def gcd2(m,n):
+    print("Results from gcd2")
+    for i in range(min(m,n), 1):
+        if (m % i) == 0 & (n % i) == 0:
+            print(i)
+            
+gcd2(21, 63)
+gcd2(9999, 100000)
+gcd2(20000, 35608)
+        
